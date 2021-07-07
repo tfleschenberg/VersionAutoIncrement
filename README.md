a04ebfd1b1eabd9af80a001d8b90e399eef31034
Tim Fleschenberg <tim.fleschenberg@dark-link.net>
2021-07-07 17:09:42 +0200

    Projektdateien hinzufügen.

---
+++ README.md
@@ -0,0 +1,12 @@
+# VersionAutoIncrement
+
+VersionAutoIncrement is a small tool to autoincrement the build number of C# projects.
+
+## :memo: How to use the autoincrement
+
+Copy the exe to the $(ProjectDir) of your project and add the following line to your pre build command line:
+```
+$(ProjectDir)VersionAutoIncrement.exe $(ProjectDir)Properties\AssemblyInfo.cs
+```
+
+Alternatively you may copy the exe somewhere on your system and adjust the path in the pre build command line accordingly.
---
+++ VersionAutoIncrement/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+
+namespace VersionAutoIncrement
+{
+    static class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: VersionAutoIncrement.exe %1");
+                return 1;
+            }
+            
+            string filename = args[0];
+            
+            if (!File.Exists(filename))
+            {
+                Console.WriteLine("File {0} does not exist.", filename);
+                return 1;
+            }
+
+            try
+            {
+                string[] lines = File.ReadAllLines(filename);
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i].StartsWith("[assembly: AssemblyVersion"))
+                    {
+                        IncVersion(ref lines[i]);
+                    }
+                    else if (lines[i].StartsWith("[assembly: AssemblyFileVersion"))
+                    {
+                        IncVersion(ref lines[i]);
+                    }
+                }
+
+                File.WriteAllLines(filename, lines);
+                
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+                return 1;
+            }
+        }
+
+        static void IncVersion(ref string Line)
+        {
+            ushort Major = 0;
+            ushort Minor = 0;
+            ushort Build = 0;
+            ushort Revision = 0;
+
+            string[] VersionString = Line.Split('"');
+
+            if (VersionString.Length != 3)
+            {
+                Console.WriteLine("Problem with VersionString FieldCount @ " + Line);
+                return;
+            }
+            
+            string[] Version = VersionString[1].Split('.');
+
+            if (Version.Length > 0) UInt16.TryParse(Version[0], out Major);
+            if (Version.Length > 1) UInt16.TryParse(Version[1], out Minor);
+            if (Version.Length > 2) UInt16.TryParse(Version[2], out Build);
+            if (Version.Length > 3) UInt16.TryParse(Version[3], out Revision);
+
+            if (Build >= ushort.MaxValue - 1) Console.WriteLine("Max build version ({0}) reached.", Build);
+            else Build += 1;
+            
+            Line = String.Format("{0}\"{1}.{2}.{3}.{4}\"{5}", VersionString[0], Major, Minor, Build, Revision, VersionString[2]);
+        }
+    }
+}
